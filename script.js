cae8eb72c8a5d67b5d42a46c19558edc1a49b6d8
michele camera <michy.cam95@gmail.com>
2022-03-21 14:30:32 +0100

    create repo and init

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+let cars = [
+    {
+        nome: "Wayne Barnett",
+        "type": "minivan",
+        "registration": new Date('2017-01-03'),
+        "capacity": 7
+    },
+    {
+        "color": "red",
+        "type": "station wagon",
+        "registration": new Date('2018-03-03'),
+        "capacity": 5
+    },
+    {
+        ...
+    },
+    ...
+  ]
+
+
+
+
+
+
